b822a34b864691f27f5737e405971453c39ff964
darontech24 <darontech24@gmail.com>
2025-11-02 20:59:05 +0530

    Bean and Component Creation

---
+++ example1/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,15 @@
+package com.example.beans;
+
+import org.springframework.context.annotation.Bean;
+
+public class Vehicle {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ example1/src/main/java/com/example/main/Example1.java
@@ -0,0 +1,16 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Example1 {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Name of Vehicle : "+veh.getName());
+    }
+}
